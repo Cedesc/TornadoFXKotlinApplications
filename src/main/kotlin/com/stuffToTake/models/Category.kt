8f8f24155669322937de18f083ce295f83290aab
Christian <irgendwas@nix.de>
2022-04-15 09:56:42 +0200

    feat: minor changes

--- src/main/kotlin/com/stuffToTake/models/Category.kt
+++ src/main/kotlin/com/stuffToTake/models/Category.kt
@@ -1,5 +1,6 @@
 package com.stuffToTake.models
 
+// The string representations must not have any spaces at the beginning or at the end!
 enum class Category {
     ON_PC {override fun toString(): String = "Am PC hochladen"},
     NINTENDO_SWITCH {override fun toString(): String = "Sachen für Nintendo Switch"}
--- src/main/kotlin/com/stuffToTake/tests/modelsTests/EssentialItemTest.kt
+++ src/main/kotlin/com/stuffToTake/tests/modelsTests/EssentialItemTest.kt
@@ -23,4 +23,5 @@ class EssentialItemTest {
     @Test
     fun unmarkAsToTake() {
     }
+
 }
