b71ffef41cbefdc738c3cc7d86055164567f60ed
Cedesc <cdohme01@students.uni-mainz.de>
2022-05-23 13:53:19 +0200

    feat: filled Category.kt and adjusted the to do list

--- src/main/kotlin/com/stuffToTake/models/Category.kt
+++ src/main/kotlin/com/stuffToTake/models/Category.kt
@@ -2,13 +2,11 @@ package com.stuffToTake.models
 
 // The string representations must not have any spaces at the beginning or at the end!
 enum class Category {
+    TO_CARRY {override fun toString(): String = "To Carry"},
     ON_PC {override fun toString(): String = "Upload on PC"},
     GAME_CONSOLES_STUFF {override fun toString(): String = "Game Consoles Stuff"},
     CLOTHING {override fun toString(): String = "Clothing"},
-    // PS4 stuff (?) or console stuff (PS4 and Switch together)
-    // stuff i have to take in hands (like suitcase and bag)
-    // food (?) and beverages (?)
-    // hygiene products (?) / bathroom stuff (?) / care products (?)
-    // technical stuff
-    // uni stuff (?)
+    FOOD_AND_BEVERAGES {override fun toString(): String = "Food and Beverages"},
+    CARE_PRODUCTS {override fun toString(): String = "Care Products"},
+    TECHNICAL_STUFF {override fun toString(): String = "Technical Stuff"}
 }
