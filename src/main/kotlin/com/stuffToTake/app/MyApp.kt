c44d20719f924ddd7f3b02c43161c1f41d83bbc3
Cedesc <cdohme01@students.uni-mainz.de>
2022-05-25 16:33:39 +0200

    feat: Added minimal size of the window

--- src/main/kotlin/com/stuffToTake/app/MyApp.kt
+++ src/main/kotlin/com/stuffToTake/app/MyApp.kt
@@ -3,11 +3,20 @@ package com.stuffToTake.app
 import com.stuffToTake.controllers.ItemsListController
 import com.stuffToTake.styles.Styles
 import com.stuffToTake.view.MenuView
+import javafx.stage.Stage
 import tornadofx.App
 
 class MyApp : App(MenuView::class, Styles::class) {
 
     private val itemsListController: ItemsListController by inject()
+
+    override fun start(stage: Stage) {
+        with(stage) {
+            minWidth = 200.0
+            minHeight = 150.0
+            super.start(this)
+        }
+    }
 
     override fun stop() {
         // Save items in txt file.
