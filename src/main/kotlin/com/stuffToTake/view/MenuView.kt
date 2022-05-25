38e023c2474a86069629cf5f0f8bff06a8bc9282
Cedesc <cdohme01@students.uni-mainz.de>
2022-05-25 16:34:13 +0200

    feat: Reworked appearance of MenuView.kt

--- src/main/kotlin/com/stuffToTake/view/MenuView.kt
+++ src/main/kotlin/com/stuffToTake/view/MenuView.kt
@@ -2,6 +2,8 @@ package com.stuffToTake.view
 
 import com.stuffToTake.controllers.ItemsListController
 import com.stuffToTake.controllers.MenuController
+import javafx.scene.text.FontWeight
+import javafx.scene.text.TextAlignment
 import tornadofx.*
 
 class MenuView : View("Menu") {
@@ -9,10 +11,27 @@ class MenuView : View("Menu") {
     private val menuController: MenuController by inject()
     private val itemsController: ItemsListController by inject()
 
-    override val root = vbox {
+    override val root = gridpane {
+        // set size
+        setPrefSize(500.0, 195.0)
+
         button("Add Item") {
             action {
                 menuController.toAddItemView()
+            }
+
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(0, 0)
+                columnSpan = 2
+                marginBottom = 10.0
+            }
+            style {  // TODO repetitive, bring together
+                padding = box(10.px)
+                this.wrapText = true
+                fontSize = 15.px
+                fontWeight = FontWeight.BOLD
+                textAlignment = TextAlignment.CENTER
             }
         }
         button("Full List of \"To Mainz\"-Items") {
@@ -20,11 +39,35 @@ class MenuView : View("Menu") {
                 itemsController.changeSelectedListToMainz()
                 menuController.toListOfItemsView()
             }
+
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(0, 1)
+            }
+            style {  // TODO repetitive, bring together
+                padding = box(10.px)
+                this.wrapText = true
+                fontSize = 15.px
+                fontWeight = FontWeight.BOLD
+                textAlignment = TextAlignment.CENTER
+            }
         }
         button("Full List of \"To WW\"-Items") {
             action {
                 itemsController.changeSelectedListToWW()
                 menuController.toListOfItemsView()
+            }
+
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(1, 1)
+            }
+            style {  // TODO repetitive, bring together
+                padding = box(10.px)
+                this.wrapText = true
+                fontSize = 15.px
+                fontWeight = FontWeight.BOLD
+                textAlignment = TextAlignment.CENTER
             }
         }
         button("Choose Items for Mainz") {
@@ -32,16 +75,54 @@ class MenuView : View("Menu") {
                 itemsController.changeSelectedListToMainz()
                 menuController.toChooseItemsView()
             }
+
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(0, 2)
+            }
+            style {  // TODO repetitive, bring together
+                padding = box(10.px)
+                this.wrapText = true
+                fontSize = 15.px
+                fontWeight = FontWeight.BOLD
+                textAlignment = TextAlignment.CENTER
+            }
         }
         button("Choose Items for WW") {
             action {
                 itemsController.changeSelectedListToWW()
                 menuController.toChooseItemsView()
             }
+
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(1, 2)
+            }
+            style {  // TODO repetitive, bring together
+                padding = box(10.px)
+                this.wrapText = true
+                fontSize = 15.px
+                fontWeight = FontWeight.BOLD
+                textAlignment = TextAlignment.CENTER
+            }
         }
         button("History View") {
             action {
                 menuController.toHistoryView()
+            }
+
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(0, 3)
+                columnSpan = 2
+                marginTop = 10.0
+            }
+            style {  // TODO repetitive, bring together
+                padding = box(10.px)
+                this.wrapText = true
+                fontSize = 15.px
+                fontWeight = FontWeight.BOLD
+                textAlignment = TextAlignment.CENTER
             }
         }
 
