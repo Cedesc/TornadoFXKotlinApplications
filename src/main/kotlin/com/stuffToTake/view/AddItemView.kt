0aea2de58e52b289f9b62119c6813d008004e783
Christian <irgendwas@nix.de>
2022-06-07 16:00:00 +0200

    feat: deleted OwnButton.kt

--- src/main/kotlin/com/stuffToTake/view/AddItemView.kt
+++ src/main/kotlin/com/stuffToTake/view/AddItemView.kt
@@ -40,13 +40,8 @@ class AddItemView : View("Add Item") {
         fieldset {
             hbox {
                 checkbox("To Take", itemToTake)
-
                 checkbox("To Mainz", toMainz)
-
                 checkbox("To WW", toWW)
-
-                add(OwnButton("To Mainz"))
-
             }
         }
 
