6d4b627abffaaa6ab5a60b2f7101bc97308b8085
Christian <irgendwas@nix.de>
2022-06-08 10:39:40 +0200

    feat: overworked ListOfItemsView.kt

--- src/main/kotlin/com/stuffToTake/view/ListOfItemsView.kt
+++ src/main/kotlin/com/stuffToTake/view/ListOfItemsView.kt
@@ -5,8 +5,10 @@ import com.stuffToTake.controllers.MenuController
 import com.stuffToTake.models.ShowItem
 import javafx.beans.property.SimpleObjectProperty
 import javafx.collections.ObservableList
+import javafx.geometry.Pos
 import javafx.scene.control.SelectionMode
 import javafx.scene.paint.Color
+import javafx.scene.text.FontWeight
 import tornadofx.*
 
 
@@ -18,19 +20,34 @@ class ListOfItemsView : View("List of Items") {
     private val selectedObservableItemsList: SimpleObjectProperty<ObservableList<ShowItem>> =
         SimpleObjectProperty(itemsListController.selectedItemList.observableShowItems)
 
+    private val ownPrefWidth: Double = 500.0
+    private val ownPrefHeight: Double = 900.0
+
 
     override val root = vbox {
+        // set size
+        setPrefSize(ownPrefWidth, ownPrefHeight)
+
         tableview(selectedObservableItemsList) {
             id = "itemsList"
-            column("Name", ShowItem::nameProperty)
-            column("Amount", ShowItem::amountProperty)
             column("ToTake", ShowItem::toTakeProperty).cellFormat {
                 text = "■"
                 style {
+                    alignment = Pos.CENTER
                     textFill = if (it)
                         Color.GREEN
                     else
                         Color.RED
+                }
+            }
+            column("Name", ShowItem::nameProperty) {
+                style {
+                    enableTextWrap()
+                }
+            }
+            column("Amount", ShowItem::amountProperty) {
+                style {
+                    alignment = Pos.CENTER
                 }
             }
             column("Categories", ShowItem::categoriesProperty).cellFormat {
@@ -41,18 +58,31 @@ class ListOfItemsView : View("List of Items") {
             }
 
             selectionModel.selectionMode = SelectionMode.SINGLE
-
-
             onUserSelect(clickCount = 2) { item ->
                 itemsListController.toEditItemView(item)
             }
 
             smartResize()
+
+
+            prefHeight = ownPrefHeight - (ownPrefHeight / 13)
+            style {
+                fontSize = 15.px
+            }
         }
 
         button("Back") {
             action {
                 menuController.backToMenuView()
+            }
+
+            prefWidth = ownPrefWidth
+            prefHeight = ownPrefHeight / 13
+            style {
+                alignment = Pos.CENTER
+                padding = box(10.px)
+                fontSize = 15.px
+                fontWeight = FontWeight.BOLD
             }
         }
 
