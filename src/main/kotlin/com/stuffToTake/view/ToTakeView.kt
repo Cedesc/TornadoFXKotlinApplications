7c41467ca2718ad8cd4f43e641ba8be7efcabba8
Christian <irgendwas@nix.de>
2022-06-08 10:33:25 +0200

    feat: overworked ToTakeView.kt

--- src/main/kotlin/com/stuffToTake/view/ToTakeView.kt
+++ src/main/kotlin/com/stuffToTake/view/ToTakeView.kt
@@ -5,7 +5,9 @@ import com.stuffToTake.controllers.ToTakeController
 import com.stuffToTake.models.ShowItem
 import javafx.beans.property.SimpleObjectProperty
 import javafx.collections.ObservableList
+import javafx.geometry.Pos
 import javafx.scene.control.SelectionMode
+import javafx.scene.text.FontWeight
 import tornadofx.*
 
 class ToTakeView : View("To Take View") {
@@ -16,12 +18,27 @@ class ToTakeView : View("To Take View") {
     private val items: SimpleObjectProperty<ObservableList<ShowItem>> =
         SimpleObjectProperty(toTakeController.itemsListToTake)
 
+    private val ownPrefWidth: Double = 300.0
+    private val ownPrefHeight: Double = 800.0
+
+
     override val root = vbox {
+        // set size
+        setPrefSize(ownPrefWidth, ownPrefHeight)
+
         tableview(items) {
             id = "toTakeList"
             column("To Take", ShowItem::toTakeProperty).useCheckbox()
-            column("Name", ShowItem::nameProperty)
-            column("Amount", ShowItem::amountProperty)
+            column("Name", ShowItem::nameProperty) {
+                style {
+                    enableTextWrap()
+                }
+            }
+            column("Amount", ShowItem::amountProperty) {
+                style {
+                    alignment = Pos.CENTER
+                }
+            }
 
             selectionModel.selectionMode = SelectionMode.SINGLE
 
@@ -30,19 +47,44 @@ class ToTakeView : View("To Take View") {
                 selectionModel.select(null)
             }
 
-
             smartResize()
-        }
 
-        button("Finished") {
-            action {
-                toTakeController.finish()
+
+            prefHeight = ownPrefHeight - (ownPrefHeight / 13)
+            style {
+                fontSize = 15.px
             }
         }
 
-        button("Back") {
-            action {
-                toTakeController.closeToTakeView()
+        hbox {
+            button("Back") {
+                action {
+                    toTakeController.closeToTakeView()
+                }
+
+                prefWidth = ownPrefWidth / 2
+                prefHeight = ownPrefHeight / 13
+                style {
+                    alignment = Pos.CENTER
+                    padding = box(10.px)
+                    fontSize = 15.px
+                    fontWeight = FontWeight.BOLD
+                }
+            }
+
+            button("Finished") {
+                action {
+                    toTakeController.finish()
+                }
+
+                prefWidth = ownPrefWidth / 2
+                prefHeight = ownPrefHeight / 13
+                style {
+                    alignment = Pos.CENTER
+                    padding = box(10.px)
+                    fontSize = 15.px
+                    fontWeight = FontWeight.BOLD
+                }
             }
         }
     }
