4b6c8a31085744248d1993ad14a89bfe890373ca
Christian <irgendwas@nix.de>
2022-06-08 11:06:01 +0200

    feat: overworked ConfirmView.kt

--- src/main/kotlin/com/stuffToTake/view/ConfirmView.kt
+++ src/main/kotlin/com/stuffToTake/view/ConfirmView.kt
@@ -1,18 +1,35 @@
 package com.stuffToTake.view
 
 import com.stuffToTake.controllers.ConfirmController
-import com.stuffToTake.models.AbstractItem
+import javafx.geometry.Pos
+import javafx.scene.text.FontWeight
+import javafx.scene.text.TextAlignment
 import tornadofx.*
 
 class ConfirmView : View("PLACEHOLDER") {
 
     private val confirmController: ConfirmController by inject()
 
+    private val ownPrefWidth: Double = 300.0
+    private val ownPrefHeight: Double = 180.0
+
 
     override val root = vbox {
+        // set size
+        setPrefSize(ownPrefWidth, ownPrefHeight)
 
         label {
             bind(confirmController.text)
+
+            prefHeight = ownPrefHeight * 3 / 4
+            style {
+                alignment = Pos.CENTER
+                textAlignment = TextAlignment.CENTER
+                padding = box(10.px)
+                fontSize = 18.px
+                fontWeight = FontWeight.BOLD
+                wrapText = true
+            }
         }
 
         hbox {
@@ -20,10 +37,28 @@ class ConfirmView : View("PLACEHOLDER") {
                 action {
                     confirmController.setToYes()
                 }
+
+                prefWidth = ownPrefWidth / 2
+                prefHeight = ownPrefHeight / 4
+                style {
+                    alignment = Pos.CENTER
+                    padding = box(10.px)
+                    fontSize = 15.px
+                    fontWeight = FontWeight.BOLD
+                }
             }
             button("No") {
                 action {
                     confirmController.setToNo()
+                }
+
+                prefWidth = ownPrefWidth / 2
+                prefHeight = ownPrefHeight / 4
+                style {
+                    alignment = Pos.CENTER
+                    padding = box(10.px)
+                    fontSize = 15.px
+                    fontWeight = FontWeight.BOLD
                 }
             }
         }
