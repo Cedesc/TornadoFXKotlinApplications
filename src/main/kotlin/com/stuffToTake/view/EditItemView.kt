268d4f0cd666e703a3fc0918af29fd0968b033e1
Christian <irgendwas@nix.de>
2022-06-08 10:49:09 +0200

    feat: overworked EditItemView.kt

--- src/main/kotlin/com/stuffToTake/view/EditItemView.kt
+++ src/main/kotlin/com/stuffToTake/view/EditItemView.kt
@@ -5,8 +5,10 @@ import com.stuffToTake.controllers.ItemsListController
 import com.stuffToTake.models.*
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleStringProperty
+import javafx.geometry.Pos
 import javafx.scene.control.ListView
 import javafx.scene.control.SelectionMode
+import javafx.scene.text.FontWeight
 import tornadofx.*
 
 class EditItemView : View("PLACEHOLDER") {
@@ -23,23 +25,115 @@ class EditItemView : View("PLACEHOLDER") {
     private val itemToTake = model.bind { SimpleBooleanProperty() }
     private val categoriesListView = ListView(Category.values().toList().toObservable())
 
+    private val ownPrefWidth: Double = 500.0
+    private val ownPrefHeight: Double = 350.0
 
-    override val root = vbox {
+
+    override val root = gridpane {
+        // set size
+        setPrefSize(ownPrefWidth, ownPrefHeight)
+
         fieldset("Name") {
             textfield(itemName).required()
+
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(0, 1)
+            }
+            style {
+                setPrefWidth(ownPrefWidth / 2)
+            }
+            // only affects the text "Name"
+            this.children[0].style {
+                fontWeight = FontWeight.BOLD
+            }
         }
+
         fieldset("Amount") {
             textfield(itemAmount)
+
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(0, 2)
+            }
+            style {
+                setPrefWidth(ownPrefWidth / 2)
+            }
+            // only affects the text "Name"
+            this.children[0].style {
+                fontWeight = FontWeight.BOLD
+            }
         }
+
         fieldset("Type") {
             combobox(itemType, observableListOf("Essential Item", "Optional Item", "One Time Item")).required()
+
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(0, 3)
+            }
+            style {
+                setPrefWidth(ownPrefWidth / 2)
+            }
+            // only affects the text "Name"
+            this.children[0].style {
+                fontWeight = FontWeight.BOLD
+            }
         }
+
         fieldset("Categories") {
             add(categoriesListView)
+
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(1, 1)
+                rowSpan = 3
+            }
+            style {
+                setPrefWidth(ownPrefWidth / 2)
+            }
+            // only affects the text "Name"
+            this.children[0].style {
+                fontWeight = FontWeight.BOLD
+            }
         }
-        fieldset {
-            hbox {
-                checkbox("To Take", itemToTake)
+
+
+        checkbox("To Take", itemToTake) {
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(0, 4)
+                columnSpan = 2
+            }
+            style {
+                alignment = Pos.CENTER
+                padding = box(10.px)
+                fontWeight = FontWeight.BOLD
+            }
+        }
+
+
+        button("Delete") {
+            action {
+                // Show window to check if user is sure
+                confirmController.confirmDeletion(item)
+
+                runLater {
+                    // wait until the pop-up window is closed
+                    find(ConfirmView::class).whenUndockedOnce {
+                        // if the confirmation is given, apply the changes
+                        if (confirmController.confirmed)
+                            itemsListController.deleteItem(item)
+                    }
+                }
+            }
+
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(0, 5)
+            }
+            style {
+                fontWeight = FontWeight.BOLD
             }
         }
 
@@ -61,22 +155,19 @@ class EditItemView : View("PLACEHOLDER") {
                     }
                 }
             }
+
+            useMaxSize = true
+            gridpaneConstraints {
+                columnRowIndex(1, 5)
+            }
+            style {
+                fontWeight = FontWeight.BOLD
+            }
         }
 
-        button("Delete") {
-            action {
-                // Show window to check if user is sure
-                confirmController.confirmDeletion(item)
-
-                runLater {
-                    // wait until the pop-up window is closed
-                    find(ConfirmView::class).whenUndockedOnce {
-                        // if the confirmation is given, apply the changes
-                        if (confirmController.confirmed)
-                            itemsListController.deleteItem(item)
-                    }
-                }
-            }
+        // style attributes that are applied to every child
+        children.style(append = true) {
+            fontSize = 15.px
         }
 
     }
